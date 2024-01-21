e60c4ebc07c77da64e20e63bed3a95db688cba9d
devruchoa <ronaldo.uchoa.pessoal@gmail.com>
2024-01-21 07:23:25 -0300

    chore: initial config

---
+++ src/main/java/com/uchoa/iocdijavaspringbootpoc/IocDiJavaspringbootPocApplication.java
@@ -0,0 +1,13 @@
+package com.uchoa.iocdijavaspringbootpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IocDiJavaspringbootPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IocDiJavaspringbootPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uchoa/iocdijavaspringbootpoc/IocDiJavaspringbootPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uchoa.iocdijavaspringbootpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IocDiJavaspringbootPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
